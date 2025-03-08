0869ee2752ad857e2cfedc609b11e1a8434fb9a9
razuahmedjoy <rjjoybzs420@gmail.com>
2025-03-08 11:31:48 +0600

    changed frontend for vercel deployment

--- tic-tac-toe-frontend/vite.config.ts
+++ tic-tac-toe-frontend/vite.config.ts
@@ -5,10 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // Ensure the output folder is correct
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://tic-tac-toe-5-x5-3-p.vercel.app',
         changeOrigin: true,
       }
     }
